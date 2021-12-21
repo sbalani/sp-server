37166b68d33d43cf6d9e21a12fb1600f6199832a
sbalani <sbalani@gmail.com>
2021-12-21 19:14:50 +0700

    fixed db.js pool to heroku variables

--- config/db.js
+++ config/db.js
@@ -2,12 +2,24 @@ require("dotenv").config();
 const mysql = require("mysql2");
 //const { DatabaseError } = require('pg');
 
+/*
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
 });
+*/
+
+const pool = mysql.createPool({
+    host: "l6glqt8gsx37y4hs.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    user: "pxcdgwoth0ir2mjk",
+    database: "a8r1ltvklmu2edni",
+    password: "mnmh0ex5x8cn9z24",
+});
+
+
+
 
 let sql = "SELECT * FROM battledata.history WHERE battle_queue_id = 'c9d3b611e40d2411a35d44009359b37e0cf2fb06';";
 
